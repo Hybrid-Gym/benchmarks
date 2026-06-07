8ce93995b1da73b277a4a4243eff4d329e4c834a
yiqingxyq <yiqingxi@andrew.cmu.edu>
2026-06-07 17:40:04 -0400

    update script: use remote runtime instead

--- benchmarks/swebench/scripts/test_infer.sh
+++ benchmarks/swebench/scripts/test_infer.sh
@@ -1,9 +1,9 @@
 MODEL_NAME="${1:-qwen25-coder-7b-func-localize-claude47-1467i-5e-0-00005lr-bs16-bf16}"
 
-export OPENHANDS_EVAL_AGENT_SERVER_IMAGE="ghcr.io/yiqingxyq/eval-agent-server"
+export OPENHANDS_EVAL_AGENT_SERVER_IMAGE="ghcr.io/hybrid-gym/eval-agent-server"
 
 # Required: Your runtime API key
-# export RUNTIME_API_KEY=$REMOTE_KEY
+export RUNTIME_API_KEY=$REMOTE_KEY
 
 # Optional: Override default runtime API URL
 # export RUNTIME_API_URL="https://runtime.eval.all-hands.dev" 
@@ -12,8 +12,8 @@ uv run swebench-infer .llm_config/${MODEL_NAME}.json \
     --dataset princeton-nlp/SWE-bench_Verified \
     --select benchmarks/swebench/easy50_instances.txt \
     --split test \
-    --workspace apptainer \
-    --num-workers 8 \
+    --workspace remote \
+    --num-workers 16 \
     --max-iterations 60 \
     --output-dir $STORAGE_DIR/benchmarks/evaluation_outputs/swe_bench_easy50_outputs
 
