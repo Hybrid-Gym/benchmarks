5ce73dd16c8eac3d77e7a2ab0e248e8925bb0c75
yiqingxyq <yiqingxi@andrew.cmu.edu>
2026-06-08 19:00:37 -0400

    add eval scripts

--- benchmarks/swebench/scripts/test_infer.sh
+++ benchmarks/swebench/scripts/test_infer.sh
@@ -1,8 +1,10 @@
 MODEL_NAME="${1:-qwen25-coder-7b-func-localize-claude47-1467i-5e-0-00005lr-bs16-bf16}"
 
-export OPENHANDS_EVAL_AGENT_SERVER_IMAGE="ghcr.io/hybrid-gym/eval-agent-server"
+export SDK_SHORT_SHA="e212d45"
+export IMAGE_TAG_PREFIX="e212d45-35d813f"
 
-# Required: Your runtime API key
+export OPENHANDS_EVAL_AGENT_SERVER_IMAGE="ghcr.io/hybrid-gym/eval-agent-server"
+export EVAL_AGENT_SERVER_IMAGE="ghcr.io/hybrid-gym/eval-agent-server"
 export RUNTIME_API_KEY=$REMOTE_KEY
 
 # Optional: Override default runtime API URL
@@ -18,3 +20,13 @@ uv run swebench-infer .llm_config/${MODEL_NAME}.json \
     --output-dir $STORAGE_DIR/benchmarks/evaluation_outputs/swe_bench_easy50_outputs
 
     # --n-limit 1
+
+OUTPUT_FILE=$STORAGE_DIR/benchmarks/evaluation_outputs/swe_bench_easy50_outputs/princeton-nlp__SWE-bench_Verified-test/openai/${MODEL_NAME}_sdk_${SDK_SHORT_SHA}_maxiter_60/output.jsonl
+
+# uv run swebench-eval $OUTPUT_FILE --run-id init --modal
+
+export OGMA_STORAGE_DIR="/projects/ogma3/yiqingxi"
+export OGMA_OUTPUT_DIR="${OGMA_STORAGE_DIR}/benchmarks/evaluation_outputs/swe_bench_easy50_outputs/princeton-nlp__SWE-bench_Verified-test/openai/${MODEL_NAME}_sdk_${SDK_SHORT_SHA}_maxiter_60"
+
+rclone copy $OUTPUT_FILE ogma:$OGMA_OUTPUT_DIR
+
